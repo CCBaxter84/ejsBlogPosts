e75e641a77cefd10930847472d9b63cee3a06676
CCBaxter84 <troyceclark@gmail.com>
2024-02-17 13:28:45 -0700

    Fixed styling issue with img

--- server/index.js
+++ server/index.js
@@ -12,6 +12,6 @@ app.use(express.static(__dirname + "/../public"))
 app.use(express.json())
 app.use("/posts", postsRouter)
 
-app.listen(8080)
-console.log('Server is listening on port 8080')
+app.listen(8081)
+console.log('Server is listening on port 8081')
 
